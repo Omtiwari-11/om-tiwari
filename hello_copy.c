3576078ae63b8bea261009c3a4d7f4198a5457fa
om tiwari <omtiwari888973@gmail.com>
2025-09-02 15:55:21 +0530

    msg

---
+++ hello_copy.c
@@ -0,0 +1,36 @@
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World");
+// }
+
+#include<stdio.h>
+int main() 
+{
+
+for(int i=1;i<=10;i++)
+{
+printf("hello\n");
+// printf("%d\n",i*2);
+// iff(i%2==1){
+// printf("%d\n",i);
+
+}
+
+
+
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
